f1b938cc1228a0d795bb8b8f6977017e9b3c0839
Park Jae Seong <timer973@naver.com>
2025-08-15 22:04:07 +0900

    Entity 구현

---
+++ src/main/java/com/sample/dayone/persist/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.sample.dayone.persist;
+
+import com.sample.dayone.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+}
---
+++ src/main/java/com/sample/dayone/persist/DividendRepository.java
@@ -0,0 +1,9 @@
+package com.sample.dayone.persist;
+
+import com.sample.dayone.persist.entity.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+}
