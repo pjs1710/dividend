00d3b51ab5b1ad044b4fbacb1bdcf4248b064be7
Park Jae Seong <timer973@naver.com>
2025-08-16 19:48:27 +0900

    회사 조회

--- src/main/java/com/sample/dayone/web/CompanyController.java
+++ src/main/java/com/sample/dayone/web/CompanyController.java
@@ -1,11 +1,16 @@
 package com.sample.dayone.web;
 
 import com.sample.dayone.model.Company;
+import com.sample.dayone.persist.entity.CompanyEntity;
 import com.sample.dayone.service.CompanyService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/company")
@@ -20,8 +25,9 @@ public class CompanyController {
     }
 
     @GetMapping
-    public ResponseEntity<?> searchCompany() {
-        return null;
+    public ResponseEntity<?> searchCompany(final Pageable pageable) {
+        Page<CompanyEntity> companies = this.companyService.getAllCompany(pageable);
+        return ResponseEntity.ok(companies);
     }
 
     @PostMapping
