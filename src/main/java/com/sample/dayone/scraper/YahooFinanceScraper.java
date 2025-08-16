53ff48b4c116a4314575ef01755153fe97ed8150
Park Jae Seong <timer973@naver.com>
2025-08-16 19:01:12 +0900

    배당금 저장

--- src/main/java/com/sample/dayone/scraper/YahooFinanceScraper.java
+++ src/main/java/com/sample/dayone/scraper/YahooFinanceScraper.java
@@ -3,6 +3,7 @@ package com.sample.dayone.scraper;
 import com.sample.dayone.model.Company;
 import com.sample.dayone.model.Dividend;
 import com.sample.dayone.model.ScrapedResult;
+import com.sample.dayone.model.constants.Month;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,21 +11,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class YahooFinanceScraper {
 
     private static final String URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
+    private static final long START_TIME = 86400; // 60 * 60 * 24
 
     public ScrapedResult scrap(Company company) {
         var scrapResult = new ScrapedResult();
         scrapResult.setCompany(company);
 
         try {
-            long start = 0;
-            long end = 0;
-            String url = String.format(URL, company.getTicker(), start, end);
+            long now = System.currentTimeMillis();
+
+            String url = String.format(URL, company.getTicker(), START_TIME, now);
             Connection connection = Jsoup.connect(url);
             Document document = connection.get();
 
@@ -39,17 +42,28 @@ public class YahooFinanceScraper {
                     continue;
                 }
                 String[] splits = text.split(" ");
-                String month = splits[0];
+                int month = Month.strToNumber(splits[0]);
                 int day = Integer.valueOf(splits[1].replace(",", ""));
                 int year = Integer.valueOf(splits[2]);
                 String dividend = splits[3];
 
+                if (month < 0) {
+                    throw new RuntimeException("Unexpected Month enum value -> " + splits[0]);
+                }
 
+                dividends.add(Dividend.builder()
+                        .date(LocalDateTime.of(year, month, day, 0, 0))
+                        .dividend(dividend)
+                        .build());
             }
             scrapResult.setDividendEntities(dividends);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return scrapResult;
+    }
+
+    public Company scrapCompanyByTicker(String ticker) {
         return null;
     }
 }
