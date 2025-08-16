61275eb09dee61af4052ec1f450bc53f976c7f76
Park Jae Seong <timer973@naver.com>
2025-08-17 05:05:10 +0900

    Redis Cache 구현

--- src/main/java/com/sample/dayone/model/Company.java
+++ src/main/java/com/sample/dayone/model/Company.java
@@ -1,11 +1,14 @@
 package com.sample.dayone.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
 
     private String ticker;
--- src/main/java/com/sample/dayone/service/FinanceService.java
+++ src/main/java/com/sample/dayone/service/FinanceService.java
@@ -8,12 +8,15 @@ import com.sample.dayone.persist.DividendRepository;
 import com.sample.dayone.persist.entity.CompanyEntity;
 import com.sample.dayone.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
@@ -21,22 +24,18 @@ public class FinanceService {
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
 
+    @Cacheable(key = "#companyName", value = "finance")
     public ScrapedResult getDividendByCompanyName(String companyName) {
+        log.info("search company -> " + companyName);
         CompanyEntity company = this.companyRepository.findByName(companyName)
                     .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다."));
 
         List<DividendEntity> dividendEntities = this.dividendRepository.findAllByCompanyId(company.getId());
 
         List<Dividend> dividends = dividendEntities.stream()
-                .map(e -> Dividend.builder()
-                        .date(e.getDate())
-                        .dividend(e.getDividend())
-                        .build())
+                .map(e -> new Dividend(e.getDate(), e.getDividend()))
                 .collect(Collectors.toList());
 
-        return new ScrapedResult(Company.builder()
-                .ticker(company.getTicker())
-                .name(company.getName())
-                .build(), dividends);
+        return new ScrapedResult(new Company(company.getTicker(), company.getName()), dividends);
     }
 }
