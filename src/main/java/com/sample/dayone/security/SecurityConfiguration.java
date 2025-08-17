6b9895a5029db46505ff0be2e6a7ed52507f201a
Park Jae Seong <timer973@naver.com>
2025-08-17 16:15:10 +0900

    회원 관리 기능 구현

--- src/main/java/com/sample/dayone/security/SecurityConfiguration.java
+++ src/main/java/com/sample/dayone/security/SecurityConfiguration.java
@@ -1,4 +1,32 @@
 package com.sample.dayone.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**/signup", "/**/signin").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
 }
--- src/main/java/com/sample/dayone/security/TokenProvider.java
+++ src/main/java/com/sample/dayone/security/TokenProvider.java
@@ -1,11 +1,15 @@
 package com.sample.dayone.security;
 
+import com.sample.dayone.service.MemberService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -18,6 +22,8 @@ public class TokenProvider {
 
     private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
     private static final String KEY_ROLES = "roles";
+
+    private final MemberService memberService;
 
     @Value("${spring.jwt.secret}")
     private String secretKey;
@@ -41,6 +47,11 @@ public class TokenProvider {
                 .setExpiration(expiredDate)  // 토큰 만료 시간
                 .signWith(SignatureAlgorithm.HS512, this.secretKey)  // 사용할 암호화 알고리즘, 비밀키
                 .compact();
+    }
+
+    public Authentication getAuthentication(String jwt) {
+        UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUsername(String token) {
